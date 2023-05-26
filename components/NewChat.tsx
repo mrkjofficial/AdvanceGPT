9385b3d879e002401ce516768f800be1349aebb3
Karan Jaiswal <karanjaiswal0000@gmail.com>
2023-05-27 00:10:52 +0530

    Commit @ 27-05-2023

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { collection, orderBy, query } from "firebase/firestore";
 import { useEffect, useRef } from "react";
+import { BoltIcon, ExclamationTriangleIcon, SunIcon } from "@heroicons/react/24/outline";
 
 type Props = {
 	chatId: string;
@@ -31,12 +32,56 @@ const NewChat = ({ chatId }: Props) => {
 	}, [messages]);
 
 	return (
-		<div className="new-chat__container">
-			{messages?.docs.map((message) => (
-				<Message key={message.id} message={message.data()} />
-			))}
-			<div ref={messagesEndRef} />
-		</div>
+		<>
+			{messages?.docs.length === 0 && (
+				<div className="home__container">
+					<h1 className="home__heading">AdvanceGPT</h1>
+					<div className="home__info-container">
+						<div className="home__info-wrapper">
+							<div className="home__info">
+								<SunIcon className="home__info-icon" />
+								<h2>Examples</h2>
+							</div>
+							<div className="home__info-group">
+								<button className="home__info-button">{`"Explain quantum computing in simple terms" →`}</button>
+								<button className="home__info-button">{`"Got any creative ideas for a 10 year old's birthday?" →`}</button>
+								<button className="home__info-button">{`"How do I make an HTTP request in Javascript?" →`}</button>
+							</div>
+						</div>
+						<div className="home__info-wrapper">
+							<div className="home__info">
+								<BoltIcon className="home__info-icon" />
+								<h2>Capabilities</h2>
+							</div>
+							<div className="home__info-group">
+								<p className="home__info-text">{`Remembers what user said earlier in the conversation`}</p>
+								<p className="home__info-text">{`Allows user to provide follow-up corrections`}</p>
+								<p className="home__info-text">{`Trained to decline inappropriate requests`}</p>
+							</div>
+						</div>
+						<div className="home__info-wrapper">
+							<div className="home__info">
+								<ExclamationTriangleIcon className="home__info-icon" />
+								<h2>Limitations</h2>
+							</div>
+							<div className="home__info-group">
+								<p className="home__info-text">{`May occasionally generate incorrect information`}</p>
+								<p className="home__info-text">{`May occasionally produce harmful instructions or biased content`}</p>
+								<p className="home__info-text">{`Limited knowledge of world and events after 2021`}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+			{messages?.docs.length! > 0 && (
+				<div className="new-chat__container">
+					{messages?.docs.map((message) => (
+						<Message key={message.id} message={message.data()} />
+					))}
+					<div ref={messagesEndRef} />
+				</div>
+			)}
+		</>
 	);
 };
 
