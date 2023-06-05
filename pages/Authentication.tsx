553f64a775d6896fe6fd26903e56b84adac543f0
Karan Jaiswal <karanjaiswal0000@gmail.com>
2023-06-05 22:53:45 +0530

    Commit @ 05-06-2023

--- pages/Authentication.tsx
+++ pages/Authentication.tsx
@@ -8,15 +8,10 @@ const Authentication = () => {
 		<div className="authentication__container">
 			<Image className="authentication__logo" src={gptLogo} alt="logo" width={300} height={300} priority />
 			<h4 className="authentication__welcome-message">Welcome to AdvanceGPT</h4>
-			<h4 className="authentication__info-message">Log in / Sign up with your account to continue</h4>
-			<div className="authentication__button-group">
-				<button className="authentication__button" onClick={() => signIn("google")}>
-					Log in
-				</button>
-				<button className="authentication__button" onClick={() => signIn()}>
-					Sign up
-				</button>
-			</div>
+			<h4 className="authentication__info-message">Log in with your google account to continue</h4>
+			<button className="authentication__button" onClick={() => signIn("google")}>
+				Log in
+			</button>
 		</div>
 	);
 };
